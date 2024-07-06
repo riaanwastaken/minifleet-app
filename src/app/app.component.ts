fe8ff2985f6b73ac9a622e36e08347344557c54d
Riaan Theron <kawasakiriaan@gmail.com>
2024-07-06 11:53:44 +0200

    firestore/angular setup complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'minifleet-web-app';
+  users$: Observable<any[]>;
 
+  constructor(private firestore: Firestore) {
+    const usersCollection = collection(this.firestore, 'main/app/users');
+    this.users$ = collectionData(usersCollection);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCql4LqrFHfqDh_F8rE-NgS_lC3hlsQG1E",
+  authDomain: "extreme-torch-424713-g5.firebaseapp.com",
+  projectId: "extreme-torch-424713-g5",
+  storageBucket: "extreme-torch-424713-g5.appspot.com",
+  messagingSenderId: "104769806924",
+  appId: "1:104769806924:web:fce7474e8ce38df8494ae9",
+  measurementId: "G-2LZQ3KP0SS"
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
+  ]
 };
